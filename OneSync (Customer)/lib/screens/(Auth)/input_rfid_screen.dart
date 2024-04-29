54cac7a34d27e236d2f149183de55408dd185e06
nvaeili <salenivaniel@gmail.com>
2024-04-29 11:08:27 +0800

    [Feature][Ivaniel] - Input RFID Screen

--- OneSync (Customer)/lib/screens/(Auth)/input_rfid_screen.dart
+++ OneSync (Customer)/lib/screens/(Auth)/input_rfid_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_svg/svg.dart'; // Ensure this package is added if using SVG images
 import 'package:onesync/screens/Dashboard/dashboard_screen.dart';
 
 class InputRfidScreen extends StatefulWidget {
+  const InputRfidScreen({Key? key}) : super(key: key);
+
   @override
   _InputRfidScreenState createState() => _InputRfidScreenState();
 }
@@ -11,25 +14,91 @@ class InputRfidScreen extends StatefulWidget {
 class _InputRfidScreenState extends State<InputRfidScreen> {
   final _formKey = GlobalKey<FormState>();
   final _rfidController = TextEditingController();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Input RFID'),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text('Input RFID', style: TextStyle(color: Color(0xFF212121))),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: SvgPicture.asset(
+              'assets/OneSync_Logo.svg', // Path to your SVG file
+              height: 44, // Set your desired height
+              width: 44, // Set your desired width
+            ),
+          )
+        ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Form(
           key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextFormField(
+                controller: _firstNameController,
+                decoration: InputDecoration(
+                  labelText: 'First Name',
+                  labelStyle: TextStyle(
+                    fontFamily: 'Inter', // Adjusted to use the Inter font
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF4D4D4D),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Color(0xFFABBED1)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your first name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _lastNameController,
+                decoration: InputDecoration(
+                  labelText: 'Last Name',
+                  labelStyle: TextStyle(
+                    fontFamily: 'Inter', // Adjusted to use the Inter font
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF4D4D4D),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Color(0xFFABBED1)),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your last name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
                 controller: _rfidController,
                 decoration: InputDecoration(
-                  labelText: 'RFID',
-                  border: OutlineInputBorder(),
+                  labelText: 'Enter your reference number',
+                  labelStyle: TextStyle(
+                    fontFamily: 'Inter', // Adjusted to use the Inter font
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF4D4D4D),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(color: Color(0xFFABBED1)),
+                  ),
                 ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -39,13 +108,37 @@ class _InputRfidScreenState extends State<InputRfidScreen> {
                 },
               ),
               SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    storeRFIDInFirestore(_rfidController.text);
-                  }
-                },
-                child: Text('Submit'),
+              Container(
+                width: double.infinity,
+                height: 44,
+                decoration: BoxDecoration(
+                  color: Color(0xFF0671E0),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      storeRFIDInFirestore(
+                        _rfidController.text,
+                        _firstNameController.text,
+                        _lastNameController.text,
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent, // Button color
+                    shadowColor: Colors.transparent, // No shadow
+                  ),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -54,31 +147,22 @@ class _InputRfidScreenState extends State<InputRfidScreen> {
     );
   }
 
-  void storeRFIDInFirestore(String rfid) async {
+  void storeRFIDInFirestore(
+      String rfid, String firstName, String lastName) async {
     try {
-      // Get current user's UID
       String? uid = FirebaseAuth.instance.currentUser?.uid;
       if (uid != null) {
-        // Reference to the current user's document in Firestore
         DocumentReference userDocRef =
             FirebaseFirestore.instance.collection('Student-Users').doc(uid);
-
-        // Update RFID and Balance fields in the document
-        await userDocRef.set(
-            {
-              'rfid': rfid,
-              'balance': 0, // Set initial balance to 0
-            },
-            SetOptions(
-                merge:
-                    true)); // Use merge option to update only specified fields
-
-        // Navigate back to dashboard
+        await userDocRef.set({
+          'rfid': rfid,
+          'firstName': firstName,
+          'lastName': lastName,
+        }, SetOptions(merge: true));
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(builder: (context) => DashboardScreen()),
         );
       } else {
-        // User not found, handle the case accordingly
         print('Error: Current user not found');
       }
     } catch (e) {
