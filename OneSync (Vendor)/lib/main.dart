7d4dba6df1eafd3ac3df2862df6041756fbb8d6e
nvaeili <salenivaniel@gmail.com>
2024-05-08 21:00:30 +0800

    [Fix][Ivaniel] - Removed Debug Banner for Documentation

--- OneSync (Vendor)/lib/main.dart
+++ OneSync (Vendor)/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         '/addProduct': (context) => const AddProductScreen(),
         '/Login': (context) => LoginScreen(),
